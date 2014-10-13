c17bc9b448f1a2ec4c7bfa3874ff74cab14a529d
LiRui <arthurlirui@163.com>
2014-10-13 21:34:17 +0800

    Saber Version 1
    Fast algorithm for superpixel that submit to CVPR2015

---
+++ Saber/Saber.cpp
@@ -0,0 +1,17 @@
+// Saber.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace std;
+using namespace cv;
+int _tmain(int argc, _TCHAR* argv[])
+{
+    cout<<"Arthur"<<endl;
+    cout<<"sdfjg"<<endl;
+    string a;
+    a.append("asjdfk");
+    string b;
+    return 0;
+}
